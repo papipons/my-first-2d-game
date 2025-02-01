fe498f340b8c81335f261e73b9b28205a85a7648
Alfonse <alfonse.dev@gmail.com>
2025-02-01 21:00:53 +0800

    Refactor Camera

--- modules/camera.lua
+++ modules/camera.lua
@@ -1,16 +1,16 @@
-local Camera = require 'libs/camera'
+local CameraLib = require 'libs/camera'
 local MathHelper = require 'libs/helpers/math'
 
-local CameraManager = {}
+local Camera = {}
 
-function CameraManager:load()
-  self.cam = Camera()
+function Camera:load()
+  self.cam = CameraLib()
 end
 
-function CameraManager:update(target, virtualDimensions, bounds)
+function Camera:update(target, vDimensions, bounds)
   self.cam:lookAt(target.x, target.y)
-  self.cam.x = MathHelper.Clamp(self.cam.x, virtualDimensions.width / 2, bounds.width - virtualDimensions.width / 2)
-  self.cam.y = MathHelper.Clamp(self.cam.y, virtualDimensions.height / 2, bounds.height - virtualDimensions.height / 2)
+  self.cam.x = MathHelper.Clamp(self.cam.x, vDimensions.width / 2, bounds.width - vDimensions.width / 2)
+  self.cam.y = MathHelper.Clamp(self.cam.y, vDimensions.height / 2, bounds.height - vDimensions.height / 2)
 end
 
-return CameraManager
+return Camera
